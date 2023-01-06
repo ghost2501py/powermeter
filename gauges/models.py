d04b80df541fcba86bc91638ff33b9155f7e1d16
Daniel Ramos <ghost2501dev@gmail.com>
2023-01-06 08:07:32 -0300

    Use gettext_lazy

--- gauges/models.py
+++ gauges/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db.models import Avg, Max, Min, Sum
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 
 class Gauge(models.Model):
